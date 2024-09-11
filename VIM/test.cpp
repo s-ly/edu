aa79fc09b41c59c5a962a287cdc0defce43fce33
s-ly <sergeylysov@outlook.com>
2024-09-11 04:43:51 +0300

    vim

---
+++ VIM/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int xvar = 10;
+  int yvar = 20;
+  int yvar2 = 20;
+
+  printf("Hello, World\n");
+  printf("x = %d\n", xvar);
+  printf("y2 = %d\n", yvar2);
+
+  return 0;
+}
